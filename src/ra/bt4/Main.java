975a9b77f87f2c051451f436f043ab7a5d85c409
Nguyễn Xuân Thành <nxt1012@gmail.com>
2023-09-22 16:55:10 +0700

    upload exercises for session 07

---
+++ src/ra/bt4/Main.java
@@ -0,0 +1,9 @@
+package ra.bt4;
+
+public class Main {
+    public static void main(String[] args) {
+    Student student = new Student();
+    student.setName("Thành");
+    student.setClassName("JV230529");
+    }
+}
